61be202976c6ee40284d9f2d52623115921f8a47
Reece H. Dunn <msclrhd@gmail.com>
2023-09-04 13:32:02 +0100

    Validate that the token sequence text matches the sentence text.

--- conllutil.py
+++ conllutil.py
@@ -1,5 +1,7 @@
 # Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 
+import conllu
+
 
 def parse_filelist(filename):
     with open(filename, 'r', encoding='utf-8') as f:
@@ -8,3 +10,18 @@ def parse_filelist(filename):
             if line == '':
                 continue
             yield line
+
+
+def parse_conllu(filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+        for sent in conllu.parse_incr(f):
+            yield sent
+
+
+def get_misc(token, attr, default):
+    if 'misc' not in token:
+        return default
+    misc = token['misc']
+    if misc is None or attr not in misc:
+        return default
+    return misc[attr]
