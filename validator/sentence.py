11f7052a76bd0db9eeae2cb1a216cb4c0c9e6850
Reece H. Dunn <msclrhd@gmail.com>
2023-10-07 21:49:52 +0100

    Convert the sentence text validator to a class.

--- validator/sentence.py
+++ validator/sentence.py
@@ -1,47 +1,59 @@
 # Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 
 from validator import conllutil
+from validator.validator import Validator
 from validator.logger import log, LogLevel
 
 
-def validate_sentence_text(sent, language):
-    token_text = ""
-    word_text = ""
-    need_space = False
-    last_mwt_id = 0
-    need_mwt_space = False
-    for token in sent:
-        if type(token['id']) is int:
-            if need_space:
-                token_text += " "
-                word_text += " "
-            if last_mwt_id >= token['id']:
-                if last_mwt_id == token['id']:
-                    need_space = need_mwt_space
-                else:
-                    need_space = False
-            else:
-                need_space = conllutil.get_misc(token, 'SpaceAfter', 'Yes') == 'Yes'
-                if last_mwt_id < token['id']:
-                    token_text += token['form']
-            word_text += token['form']
-        elif '-' in token['id']:
-            if need_space:
-                token_text += " "
-                word_text += " "
-                need_space = False
-            last_mwt_id = token['id'][2]
-            need_mwt_space = conllutil.get_misc(token, 'SpaceAfter', 'Yes') == 'Yes'
-            token_text += token['form']
+class SentenceTextValidator(Validator):
+    def __init__(self, language):
+        super().__init__(language)
+        self.token_text = ""
+        self.word_text = ""
+        self.need_space = False
+        self.last_mwt_id = 0
+        self.need_mwt_space = False
 
-    if 'text' in sent.metadata:
-        if token_text != sent.metadata['text']:
-            log(LogLevel.ERROR, sent, None, "text does not match the token sequence",
-                expect=sent.metadata['text'],
-                actual=token_text)
-        if language == 'en' and word_text != sent.metadata['text']:
-            log(LogLevel.ERROR, sent, None, "text does not match the word sequence",
-                expect=sent.metadata['text'],
-                actual=word_text)
-    else:
-        log(LogLevel.ERROR, sent, None, "sentence text is missing")
+    def validate_sentence(self, sent):
+        self.token_text = ""
+        self.word_text = ""
+        self.need_space = False
+        self.last_mwt_id = 0
+        self.need_mwt_space = False
+        super().validate_sentence(sent)
+
+        if 'text' in sent.metadata:
+            if self.token_text != sent.metadata['text']:
+                log(LogLevel.ERROR, sent, None, "text does not match the token sequence",
+                    expect=sent.metadata['text'],
+                    actual=self.token_text)
+            if self.language == 'en' and self.word_text != sent.metadata['text']:
+                log(LogLevel.ERROR, sent, None, "text does not match the word sequence",
+                    expect=sent.metadata['text'],
+                    actual=self.word_text)
+        else:
+            log(LogLevel.ERROR, sent, None, "sentence text is missing")
+
+    def validate_token(self, sent, token):
+        if self.need_space:
+            self.token_text += " "
+            self.word_text += " "
+        if self.last_mwt_id >= token['id']:
+            if self.last_mwt_id == token['id']:
+                self.need_space = self.need_mwt_space
+            else:
+                self.need_space = False
+        else:
+            self.need_space = conllutil.get_misc(token, 'SpaceAfter', 'Yes') == 'Yes'
+            if self.last_mwt_id < token['id']:
+                self.token_text += token['form']
+        self.word_text += token['form']
+
+    def validate_mwt_token(self, sent, token):
+        if self.need_space:
+            self.token_text += " "
+            self.word_text += " "
+            self.need_space = False
+        self.last_mwt_id = token['id'][2]
+        self.need_mwt_space = conllutil.get_misc(token, 'SpaceAfter', 'Yes') == 'Yes'
+        self.token_text += token['form']
