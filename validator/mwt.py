8fa26d6329149ee446e4700c7fd9d2a3feabee9d
Reece H. Dunn <msclrhd@gmail.com>
2023-10-07 21:43:29 +0100

    Convert the mwt-token validator to a class.

--- validator/mwt.py
+++ validator/mwt.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 
 from validator import conllutil
+from validator.validator import Validator
 from validator.logger import log, LogLevel
 
 
@@ -20,33 +21,42 @@ def is_mwt_end(form, prev_form):
     return None
 
 
-def validate_mwt_tokens(sent, language):
-    first_mwt_id = 0
-    last_mwt_id = 0
-    prev_form = ' '
-    for token in sent:
-        if type(token['id']) is int:
-            form = token['form']
-            if last_mwt_id < token['id'] or first_mwt_id == token['id']:
-                mwt_end = is_mwt_end(form, prev_form)
-                if is_mwt_start(prev_form) and mwt_end is not None and not token['deprel'] == 'reparandum':
-                    if mwt_end == LogLevel.ERROR:
-                        log(mwt_end, sent, token,
-                            f"multi-word continuation without a multi-word token range for '{prev_form}][{form}'")
-                    else:
-                        log(mwt_end, sent, token,
-                            f"possible multi-word continuation without a multi-word token range for '{prev_form}][{form}'")
-            elif conllutil.get_misc(token, 'SpaceAfter', 'Yes') == 'No':
-                log(LogLevel.ERROR, sent, token, f"multi-word token contains a SpaceAfter=No annotation")
+class MwtTokenValidator(Validator):
+    def __init__(self, language):
+        super().__init__(language)
+        self.first_mwt_id = 0
+        self.last_mwt_id = 0
+        self.prev_form = ' '
 
-            if conllutil.get_misc(token, 'SpaceAfter', 'Yes') == 'Yes':
-                prev_form = ' '
-            elif conllutil.get_misc(token, 'CorrectSpaceAfter', 'No') == 'Yes':
-                prev_form = ' '
-            else:
-                prev_form = form
+    def validate_sentence(self, sent):
+        self.first_mwt_id = 0
+        self.last_mwt_id = 0
+        self.prev_form = ' '
+        super().validate_sentence(sent)
+
+    def validate_token(self, sent, token):
+        form = token['form']
+        if self.last_mwt_id < token['id'] or self.first_mwt_id == token['id']:
+            mwt_end = is_mwt_end(form, self.prev_form)
+            if is_mwt_start(self.prev_form) and mwt_end is not None and not token['deprel'] == 'reparandum':
+                if mwt_end == LogLevel.ERROR:
+                    log(mwt_end, sent, token,
+                        f"multi-word continuation without a multi-word token range for '{self.prev_form}][{form}'")
+                else:
+                    log(mwt_end, sent, token,
+                        f"possible multi-word continuation without a multi-word token range for '{self.prev_form}][{form}'")
+        elif conllutil.get_misc(token, 'SpaceAfter', 'Yes') == 'No':
+            log(LogLevel.ERROR, sent, token, f"multi-word token contains a SpaceAfter=No annotation")
+
+        if conllutil.get_misc(token, 'SpaceAfter', 'Yes') == 'Yes':
+            self.prev_form = ' '
+        elif conllutil.get_misc(token, 'CorrectSpaceAfter', 'No') == 'Yes':
+            self.prev_form = ' '
         else:
-            first_mwt_id = token['id'][0]
-            last_mwt_id = token['id'][2]
-            if first_mwt_id == last_mwt_id:
-                log(LogLevel.ERROR, sent, token, f"multi-word token of length 1 is redundant")
+            self.prev_form = form
+
+    def validate_mwt_token(self, sent, token):
+        self.first_mwt_id = token['id'][0]
+        self.last_mwt_id = token['id'][2]
+        if self.first_mwt_id == self.last_mwt_id:
+            log(LogLevel.ERROR, sent, token, f"multi-word token of length 1 is redundant")
