6476ca77d9c8bd1d825d7ba331bbbdaf6f4e35ef
Reece H. Dunn <msclrhd@gmail.com>
2023-10-13 19:27:01 +0100

    mwt-word: where're

--- validator/mwt.py
+++ validator/mwt.py
@@ -54,6 +54,7 @@ mwt_suffixes = {
         'they': [Token(form='they', lemma='they'), Token(form='\'re', lemma='be')],  # they are
         'we': [Token(form='we', lemma='we'), Token(form='\'re', lemma='be')],  # we are
         'what': [Token(form='what', lemma='what'), Token(form='\'re', lemma='be')],  # what are
+        'where': [Token(form='where', lemma='where'), Token(form='\'re', lemma='be')],  # where are
         'who': [Token(form='who', lemma='who'), Token(form='\'re', lemma='be')],  # who are
         'you': [Token(form='you', lemma='you'), Token(form='\'re', lemma='be')],  # you are
     },
