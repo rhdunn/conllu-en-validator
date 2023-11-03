00ba79de00713820cc3844b8171d286c8476b793
Reece H. Dunn <msclrhd@gmail.com>
2023-11-03 12:28:11 +0000

    form: fix ordinals as fractional forms

--- validator/form.py
+++ validator/form.py
@@ -63,7 +63,7 @@ fractional_word_forms = [
     "half",  # 1/2
     "quarter",  # 1/4
 ]
-fractional_word_forms.extend(fractional_word_forms)  # 1/N
+fractional_word_forms.extend(ordinal_word_forms)  # 1/N
 
 multiplicative_word_forms = [
     "once",
