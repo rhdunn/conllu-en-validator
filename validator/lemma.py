ca0a4ab1950848b60df97babf44a0ca26279ddb6
Reece H. Dunn <msclrhd@gmail.com>
2023-11-15 22:13:24 +0000

    lemma: validate RB lemmas

--- validator/lemma.py
+++ validator/lemma.py
@@ -5,6 +5,17 @@ from validator.validator import Validator
 from validator.logger import log, LogLevel
 
 
+def match_lowercase_lemma(sent, token, form, lemma):
+    if form.lower() != lemma:
+        log(LogLevel.ERROR, sent, token,
+            f"{token['xpos']} lemma '{lemma}' is not the lowercase form '{form}' text")
+
+
+lemma_validators = {
+    'RB': match_lowercase_lemma,  # adverb
+}
+
+
 class TokenLemmaValidator(Validator):
     def __init__(self, language):
         super().__init__(language)
@@ -12,9 +23,12 @@ class TokenLemmaValidator(Validator):
     def validate_token(self, sent, token):
         form = conllutil.normalized_form(token)
         lemma = token['lemma']
+        xpos = token['xpos']
         if lemma is None:
             log(LogLevel.ERROR, sent, token, f"missing lemma text")
         elif form is None:
             pass  # Missing form text is reported by the 'form' validator.
+        elif xpos in lemma_validators:
+            lemma_validators[xpos](sent, token, form, lemma)
         else:
             pass
