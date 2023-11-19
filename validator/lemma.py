1edd6c4a09d80648905d78566e62d2d747157b72
Reece H. Dunn <msclrhd@gmail.com>
2023-11-19 20:20:15 +0000

    lemma: validate JJS lemmas -- -oaCest => oaC stemming rule

--- validator/lemma.py
+++ validator/lemma.py
@@ -74,7 +74,7 @@ comparative_adjective_stemming_rules = [
 ]
 
 superlative_adjective_stemming_rules = [
-    (re.compile(r'(ea[^aeiou])est$'), r'\1'),  # eaCest -> eaC
+    (re.compile(r'([eo]a[^aeiou])est$'), r'\1'),  # oaCest -> oaC ; eaCest -> eaC
     (re.compile(r'([ai][^aeiou]e)st$'), r'\1'),  # aCest -> aCe ; iCest -> iCe
     (re.compile(r'([dgnt])\1est'), r'\1'),  # CCest -> C
     ('iest', 'y'),
